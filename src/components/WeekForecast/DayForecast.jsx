d201e13a023a658c18318aca58e48adfb4f5c85c
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-10-25 16:42:36 +0100

    added styles #5

--- src/components/WeekForecast/DayForecast.jsx
+++ src/components/WeekForecast/DayForecast.jsx
@@ -1,10 +1,36 @@
+import date from "date-and-time";
 import React from "react";
+import "./DayForecast.css";
+
+const capitalizeFirstLetters = (string) => {
+	let arr = string.split(" ").map((word) => {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	});
+	return arr.join(" ");
+};
+
+const getDayName = (dateString) => {
+	// https://www.npmjs.com/package/date-and-time
+	return date.parse(dateString, "YYYY-MM-DD").toString().substring(0, 15);
+};
 
 const DayForecast = (props) => {
 	return (
-		<div>
-			<p>High: {props.high}</p>
-			<p>Low: {props.low}</p>
+		<div className="card">
+			<p>{getDayName(props.date)}</p>
+			<hr className="horizontal-line" />
+			<div className="high-low">
+				<p className="mg-0">High: {props.high}°C</p>
+				<p className="mg-0">Low: {props.low}°C</p>
+			</div>
+
+			<p className="mg-0 wind">Wind: {props.wind}MPH</p>
+			<img
+				src={`http://openweathermap.org/img/wn/${props.iconId}@2x.png`}
+				alt={`${capitalizeFirstLetters(props.summary)} icon`}
+			/>
+			<p className="mg-0">{capitalizeFirstLetters(props.summary)}</p>
+			<br />
 		</div>
 	);
 };
--- src/components/WeekForecast/Days.jsx
+++ src/components/WeekForecast/Days.jsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DayForecast from "./DayForecast";
+import "./Days.css";
+
+const kelvinToCelcius = (kelvin) => {
+	return parseFloat(kelvin - 273.15).toFixed(1);
+	// TODO: Try limit decimal to .5
+};
 
 const Days = () => {
 	const [days, changeDays] = useState([]);
@@ -8,22 +14,38 @@ const Days = () => {
 	const apiKey = process.env.REACT_APP_API_KEY;
 	const lat = 51.5072;
 	const lon = 0.1276;
-	const requestDays = 6;
-	let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${requestDays}&appid=${apiKey}`;
+	let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 	const fetchWeatherRequest = async () => {
 		let fetched = await axios.get(url);
+		console.log(fetched.data.list);
 		// console.log(fetched);
 		changeDays(fetched.data.list);
 	};
 
 	// Date/Day Picture SummaryText Max/Min temp Windspeed
 	const buildDays = () => {
-		let existingDays = days.map((day, i) => {
-			return (
-				<DayForecast key={i} high={day.main.temp_max} low={day.main.temp_min} />
-			);
-		});
+		let existingDays = days
+			.filter((day) => {
+				// Get the dateTime string, then check the last 8 chars (the time) against midday "12:00:00"
+				return day.dt_txt.slice(-8) === "12:00:00";
+				// Filter out all results besides midday results
+				// TODO: This is going to show the same day as today if the program is run before midday :(
+			})
+			.map((day, i) => {
+				return (
+					<DayForecast
+						key={i}
+						date={day.dt_txt.substring(0, 10)}
+						high={kelvinToCelcius(day.main.temp_max)}
+						low={kelvinToCelcius(day.main.temp_min)}
+						summary={day.weather[0].description}
+						iconId={day.weather[0].icon}
+						wind={day.wind.speed}
+						className="day-forecast"
+					/>
+				);
+			});
 		return existingDays;
 	};
 
@@ -31,7 +53,11 @@ const Days = () => {
 		fetchWeatherRequest();
 	}, []);
 
-	return <div>{buildDays()}</div>;
+	return (
+		<div>
+			<div className="day-forecast-display">{buildDays()}</div>
+		</div>
+	);
 	// return <div></div>;
 };
 
